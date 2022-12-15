a57f651f7612e1f188c557901687f81b30f10e69
JoostKraan <113507203+JoostKraan@users.noreply.github.com>
2022-12-15 22:19:06 +0100

    Added detailed class descriptions
    Added more detailed class descriptions for all three classes. also cleaned code by putting the menu text inside the while loop.

--- Adventuregame/Characterinfo.cs
+++ Adventuregame/Characterinfo.cs
@@ -17,43 +17,64 @@ namespace Adventuregame
             Console.SetCursorPosition(65, 0);
             Console.WriteLine("Archer");
             Console.SetCursorPosition(40, 4);
-            Console.WriteLine("The Archer is a class that specialises in ranged Combat,");
+            Console.WriteLine("The Archer is a class that specializes in ranged combat.");
             Console.SetCursorPosition(40, 6);
-            Console.WriteLine("They can use weapons such as an bow and arrow,Crossbows and slingshots and mainly use arrows as projectiles");
+            Console.WriteLine("They wield a variety of weapons such as bows, crossbows, and slingshots, and use arrows as their projectiles.");
             Console.SetCursorPosition(40, 8);
-            Console.WriteLine("You do have to be careful for swarms of enemies since they-");
+            Console.WriteLine("While the Archer class excels at picking off enemies from a distance,");
             Console.SetCursorPosition(40, 10);
-            Console.WriteLine("are not great at attacking multiple enemies at once.");
+            Console.WriteLine("they may struggle against swarms of foes who can quickly close the gap.");
+            Console.SetCursorPosition(40, 12);
+            Console.WriteLine("As such, they must be careful when facing multiple enemies at once.");
+            Console.ReadLine();
         }
-
         public void Sorcerer()
         {
             Console.Clear();
             Console.SetCursorPosition(65, 0);
             Console.WriteLine("Sorcerer");
             Console.SetCursorPosition(40, 4);
-            Console.WriteLine("The Sorcerer is a class that specialises in casting spells");
+            Console.WriteLine("The Sorcerer is a spell-casting class that specializes in unleashing devastating magical attacks.");
             Console.SetCursorPosition(40, 6);
-            Console.WriteLine("They have a weapon that they can use for melee attacks but mainly use spells");
+            Console.WriteLine("While they do have a weapon for melee combat, they primarily rely on their magical abilities to defeat their foes.");
             Console.SetCursorPosition(40, 8);
-            Console.WriteLine("Sorcerers use mana to use their spells, mana is a resource that they replenish over-time");
-            Console.SetCursorPosition(40, 11);
-            Console.WriteLine("Sorceres are great at attacking multiple enemies at once but are weak at close-range and single enemies.\n");
+            Console.WriteLine("Sorcerers draw their power from a resource known as mana,");
+            Console.SetCursorPosition(40, 10);
+            Console.WriteLine("which they can use to cast a variety of spells.");
+            Console.SetCursorPosition(40, 12);
+            Console.WriteLine("Over time, their mana will replenish, allowing them to continue casting spells in battle.");
+            Console.SetCursorPosition(40, 14);
+            Console.WriteLine("One of the strengths of the Sorcerer class is their ability to attack multiple enemies at once.");
+            Console.SetCursorPosition(40, 16);
+            Console.WriteLine("However, they are not as effective in close-range combat against single targets,");
+            Console.SetCursorPosition(40, 18);
+            Console.WriteLine("so they must be careful in how they approach each battle.");
             Console.SetCursorPosition(65, 13);
             Console.ReadLine();
         }
-
         public void Warrior()
         {
-            Console.WriteLine("WIP");
+            Console.Clear();
+            Console.SetCursorPosition(65, 0);
+            Console.WriteLine("Warrior");
+            Console.SetCursorPosition(40, 4);
+            Console.WriteLine("The Warrior is a class that specializes in melee combat.");
+            Console.SetCursorPosition(40, 6);
+            Console.WriteLine("They wield a variety of weapons such as axes, swords, blunt weapons, and hammers,");
+            Console.SetCursorPosition(40, 8);
+            Console.WriteLine("and are skilled at using these weapons to devastating effect in battle.");
+            Console.SetCursorPosition(40, 10);
+            Console.WriteLine("The Warrior is known for their strength and resilience,");
+            Console.SetCursorPosition(40, 12);
+            Console.WriteLine("making them difficult to take down in combat.");
+            Console.SetCursorPosition(40, 14);
+            Console.WriteLine("They excel at facing single enemies, but with the right tools and techniques,");
+            Console.SetCursorPosition(40, 16);
+            Console.WriteLine("they can effectively engage multiple foes at once.");
+            Console.SetCursorPosition(65, 18);
+            Console.ReadLine();
+
         }
-
-        public void backTomenu()
-        {
-            mainprogram
-        }
-
-
     }
 
 }
--- Adventuregame/Program.cs
+++ Adventuregame/Program.cs
@@ -6,46 +6,35 @@ namespace AdventureGame
 {
     public class Program
     {
-        
-
         static void Main(string[] args)
         {
-            Console.WriteLine("\t \t \t \t \t \t \t \t Adventure Game? \n ");
-            menu();
+            while (true)
+             {
+                Console.Clear();
+                Console.WriteLine("\t \t \t \t \t \t \t \t Adventure Game? \n ");           //not sure how to space text easier than this
+                Console.WriteLine("\t \t \t \t \t \t \t \t 1: \t Play \n \n \t \t \t \t \t \t \t \t 2: \t Help  \n \n \t \t \t \t \t \t \t \t 3: \t Exit to windows \n");
+                Console.SetCursorPosition(65, 10);
+                int userinput = Int32.Parse(Console.ReadLine());
 
-            void menu()
-            {
-
-                Console.WriteLine("\t \t \t \t \t \t \t \t 1: \t Play \n \n \t \t \t \t \t \t \t \t 2: \t Options  \n \n \t \t \t \t \t \t \t \t 3: \t Help \n \n \t \t \t \t \t \t \t \t 4: \t Exit to windows");
-                while (true)
-                 { 
-                    Console.SetCursorPosition(65, 10);
-                    int userinput = Int32.Parse(Console.ReadLine());
-
-                    switch (userinput)
-                    {
-                        case 1:
-                            Console.Clear();
-                            Game();
-                            break;
-                        case 2:
-                            Console.Clear();
-                            Options();
-                            break;
-                        case 3:
-                            Console.Clear();
-                            Help();
-                            break;
-                        case 4:
-                            Environment.Exit(0);
-                            break;
-                        default:
-                            Console.WriteLine("Invalid option");
-                            break;
-                    }
-                 }
-            }
-            void Game()
+                switch (userinput) //menu logic 
+                {
+                    case 1:
+                        Console.Clear();
+                        Game();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Help();
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+             }
+            static void Game()
             {
                 Characterinfo character = new Characterinfo();
                 Console.SetCursorPosition(65, 0);
@@ -57,9 +46,8 @@ namespace AdventureGame
                 Console.SetCursorPosition(65, 11);
                 Console.WriteLine("3: \t Warrior");
                 Console.SetCursorPosition(65, 15);
-                
-
                 int classinput = Int32.Parse(Console.ReadLine());
+
                 switch (classinput)
                 {
                     case 1:
@@ -74,16 +62,25 @@ namespace AdventureGame
                         default :
                         Console.WriteLine("Invalid Input");
                         break;
-                        
                 }
             }
-            void Options()
+            static void Help()
             {
-                Console.WriteLine("Options");
-            }
-            void Help()
-            {
-                Console.WriteLine("Help");
+                Console.SetCursorPosition(65, 0);
+                Console.WriteLine("Need help? ");
+                Console.SetCursorPosition(40, 4);
+                Console.WriteLine("To navigate in this game, you can use the keyboard and the numbered menu options");
+                Console.SetCursorPosition(40, 6);
+                Console.WriteLine("(1, 2, 3, etc.). When you start the game,");
+                Console.SetCursorPosition(40, 8);
+                Console.WriteLine("you can choose from three classes: Archer, Sorcerer, and Warrior.");
+                Console.SetCursorPosition(40, 10);
+                Console.WriteLine("Each class has its own unique abilities that affect how the game is played.");
+                Console.SetCursorPosition(40, 12);
+                Console.WriteLine("The rest of the game is currently under development.");
+                Console.SetCursorPosition(40, 18);
+                Console.WriteLine("To go back to the Main menu : Press any key...");
+                Console.ReadLine();
             }
         }
     }
