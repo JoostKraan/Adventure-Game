c2e1e386cdb05f467db30f735cb6c5b5a703531d
JoostKraan <jkraan1@st.noorderpoort.nl>
2022-12-18 17:17:15 +0100

    small changes

--- Adventuregame/Game.cs
+++ Adventuregame/Game.cs
@@ -15,21 +15,24 @@ namespace Adventuregame
             switch (characterselect)
             {
                 case 1:
-                    Console.SetCursorPosition(65, 0);
+                    Console.SetCursorPosition(130, 0);
                     Console.WriteLine("You are an Archer");
                 break;
                 case 2:
-                    Console.SetCursorPosition(65, 0);
+                    Console.SetCursorPosition(130, 0);
                     Console.WriteLine("You are a Sorcerer");
                     break;
                 case 3:
-                    Console.SetCursorPosition(65, 0);
+                    Console.SetCursorPosition(130, 0);
                     Console.WriteLine("You are an Warrior");
                     break;
-                                
-
             }
             Console.ReadLine();
+
+
+
+
+
         } 
      }
 }
